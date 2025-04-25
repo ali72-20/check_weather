fc6e40b615baaff8aed7bb62fa63c4942fbe4518
alisafwat <alisafwat11@gmail.com>
2025-04-25 17:59:28 +0300

    add: search icon add: current date and last updated

--- app/src/main/java/com/example/checkweather/MainActivity.kt
+++ app/src/main/java/com/example/checkweather/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.checkweather
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -15,11 +14,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,7 +54,7 @@ fun HomeScreenContent() {
 @Composable
 fun HomeScreenBody(modifier: Modifier = Modifier) {
     Box(
-        modifier = modifier
+        modifier = Modifier
             .fillMaxSize()
             .paint(painterResource(R.drawable.city_view), contentScale = ContentScale.Crop)
     )
@@ -63,6 +65,7 @@ fun HomeScreenBody(modifier: Modifier = Modifier) {
 
     ) {
         LocationRowView()
+        CurrentWeatherView(modifier)
     }
 }
 
@@ -71,24 +74,47 @@ fun LocationRowView() {
     Row(modifier = Modifier.wrapContentSize()) {
         Icon(
             painterResource(R.drawable.location_icon),
-            contentDescription = "Location Icon",
+            contentDescription = stringResource(R.string.location_icon),
             tint = Color.White
         )
         Text(
-            text = "Paris",
+            text = stringResource(R.string.paris),
             color = Color.White,
             fontSize = 24.sp,
             modifier = Modifier.padding(start = 4.dp, top = 4.dp)
         )
+        Spacer(modifier = Modifier.weight(1f))
         Icon(
-            contentDescription = "Search Icon",
+            contentDescription = stringResource(R.string.search_icon),
             tint = White,
             painter = painterResource(R.drawable.search_icon),
-            modifier = Modifier.padding(start = 16.)
+            modifier = Modifier.padding(end = 16.dp)
         )
     }
 }
+@Composable
+fun CurrentWeatherView(modifier: Modifier) {
+    Column(
+        Modifier.fillMaxSize().padding(top = 48.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "June 07",
+            fontSize = 40.sp,
+            color = White,
+            fontWeight = FontWeight.Bold
+        )
+        Box(
+            Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = "Updated at 6/7/2025 10:00PM",
+            color = Color.White,
+            fontSize = 16.sp
+        )
 
+    }
+}
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreenContentPreview() {
